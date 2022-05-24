090ca8eb06379761ceb8c10ac672506c8ade91a4
nowifiidie <wkahlap@gmail.com>
2022-05-24 14:08:01 +0800

    ForgetPasswordScreen Done

--- screens/ForgerPasswordScreen.js
+++ screens/ForgerPasswordScreen.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, useWindowDimensions } from 'react-native';
+import React, { useState } from 'react';
+import Logo from '../../mobileviewApp/assets/images/logo.png';
+import CustomInput from '../../mobileviewApp/components/CustomInput/CustomInput';
+import CustomButton from '../components/CustomButton';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationEvents } from 'react-navigation';
 
-const ForgerPassword = () => {
+const ForgetPassword = () => {
+  const [password, setPassword] = useState('');
+  const [password1, setPassword1] = useState('');
+
+  const { height } = useWindowDimensions();
+  const navigation = useNavigation();
+
+
+
+  const onConfirmPasswordPressed = () => {
+    console.warn("Confirm Password Reset");
+
+    navigation.navigate('Login');
+  }
+
   return (
-    <View>
-      <Text>ForgerPassword</Text>
+    <View style={styles.root}>
+      <CustomInput placeholder="Password" value={password} setValue={setPassword} secureTextEntry={true}/>
+      <CustomInput placeholder="Re-Enter Password" value={password1} setValue={setPassword1} secureTextEntry={true} />
+
+      <CustomButton text="Confirm Password" onPress={onConfirmPasswordPressed} />
+  
     </View>
   )
 }
 
-export default ForgerPassword
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  
 
-const styles = StyleSheet.create({})
+
+})
+export default ForgetPassword
+
